1f120daf40362cf55cfb94eca0f63337a2eae839
Francisco Emanuel Alberto Arroyo <85381508+ema201217@users.noreply.github.com>
2021-06-24 07:38:09 -0300

    acomode las carpetas como tenian que ir

--- index.js
+++ index.js
@@ -2,31 +2,31 @@ const express = require('express')
 const path = require('path')
 const port = 3030
 const app = express()
-app.use(express.static('public'))
+app.use(express.static('DH-Heroes/public'))
 
 app.listen(port, () => console.log(`Servidor escuchando en el puerto ${port} \n enlace de acceso al servidor : http://localhost:${port}`))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'))
+    res.sendFile(path.join(__dirname, 'DH-Heroes/views/index.html'))
 })
 app.get('/babbage', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/babbage.html'))
+    res.sendFile(path.join(__dirname, 'DH-Heroes/views/babbage.html'))
 })
 app.get('/berners-lee', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/berners-lee.html'))
+    res.sendFile(path.join(__dirname, 'DH-Heroes/views/berners-lee.html'))
 })
 app.get('/clarke', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/clarke.html'))
+    res.sendFile(path.join(__dirname, 'DH-Heroes/views/clarke.html'))
 })
 app.get('/hamilton', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/hamilton.html'))
+    res.sendFile(path.join(__dirname, 'DH-Heroes/views/hamilton.html'))
 })
 app.get('/hopper', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/hopper.html'))
+    res.sendFile(path.join(__dirname, 'DH-Heroes/views/hopper.html'))
 })
 app.get('/lovelace', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/lovelace.html'))
+    res.sendFile(path.join(__dirname, 'DH-Heroes/views/lovelace.html'))
 })
 app.get('/turing', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/turing.html'))
+    res.sendFile(path.join(__dirname, 'DH-Heroes/views/turing.html'))
 })
