a0aede53a768cad6ba8228a9a1025ce483588eac
Francisco Emanuel Alberto Arroyo <85381508+ema201217@users.noreply.github.com>
2021-06-24 02:11:57 -0300

    Subi mi trabajo practico 23/6/2021 al 24/6/2021

---
+++ index.js
@@ -0,0 +1,32 @@
+const express =require('express')
+const path = require('path')
+const port = 3030
+const app = express()
+app.use(express.static('public'))
+
+app.listen(port,()=>console.log(`Servidor escuchando en el puerto ${port} \n enlace de acceso al servidor : http://localhost:${port}`))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/index.html'))
+})
+app.get('/babbage',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/babbage.html'))
+})
+app.get('/berners-lee',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/berners-lee.html'))
+})
+app.get('/clarke',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/clarke.html'))
+})
+app.get('/hamilton',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/hamilton.html'))
+})
+app.get('/hopper',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/hopper.html'))
+})
+app.get('/lovelace',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/lovelace.html'))
+})
+app.get('/turing',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/turing.html'))
+})
